1ae2a49943e0f152ed91f7b2cef23087852bda10
MuhammadAshiqAmeer <muhammadashiqameer@gmail.com>
2024-04-20 13:48:28 +0530

    multiple file and session management added

--- examples/5_mins_rag_no_gpu/main.py
+++ examples/5_mins_rag_no_gpu/main.py
@@ -14,131 +14,136 @@
 # limitations under the License.
 
 # This is a simple standalone implementation showing rag pipeline using Nvidia AI Foundational models.
-# It uses a simple Streamlit UI and one file implementation of a minimalistic RAG pipeline.
-
-############################################
-# Component #1 - Document Loader
-############################################
-
-import streamlit as st
+# It uses a simple Streamlit UI and multiple file implementation of a minimalistic RAG pipeline, managing each session seperately.
 import os
-
-st.set_page_config(layout = "wide")
-
-with st.sidebar:
-    DOCS_DIR = os.path.abspath("./uploaded_docs")
-    if not os.path.exists(DOCS_DIR):
-        os.makedirs(DOCS_DIR)
-    st.subheader("Add to the Knowledge Base")
-    with st.form("my-form", clear_on_submit=True):
-        uploaded_files = st.file_uploader("Upload a file to the Knowledge Base:", accept_multiple_files = True)
-        submitted = st.form_submit_button("Upload!")
-
-    if uploaded_files and submitted:
-        for uploaded_file in uploaded_files:
-            st.success(f"File {uploaded_file.name} uploaded successfully!")
-            with open(os.path.join(DOCS_DIR, uploaded_file.name),"wb") as f:
-                f.write(uploaded_file.read())
-
-############################################
-# Component #2 - Embedding Model and LLM
-############################################
-
+import streamlit as st
 from langchain_nvidia_ai_endpoints import ChatNVIDIA, NVIDIAEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import DirectoryLoader
+from langchain.vectorstores import FAISS
+import pickle
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+import uuid
 
 # make sure to export your NVIDIA AI Playground key as NVIDIA_API_KEY!
 llm = ChatNVIDIA(model="mixtral_8x7b")
 document_embedder = NVIDIAEmbeddings(model="nvolveqa_40k", model_type="passage")
 query_embedder = NVIDIAEmbeddings(model="nvolveqa_40k", model_type="query")
 
-############################################
-# Component #3 - Vector Database Store
-############################################
+# Initialize SessionState to manage session-specific data
+def initialize_session_state():
+    if "session_data" not in st.session_state:
+        st.session_state.session_data = {}
 
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import DirectoryLoader
-from langchain.vectorstores import FAISS
-import pickle
+# Load vector store
+def load_vectorstore(raw_documents):
+    if "vector_file" in st.session_state.session_data:
+        vector_store_path = st.session_state.session_data["vector_file"]
+        with st.spinner("Splitting new documents into chunks..."):
+            text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+            documents = text_splitter.split_documents(raw_documents)
+        with st.spinner("Appending document chunks to vector database..."):
+            vectorstore = FAISS.from_documents(documents, document_embedder)
+        with st.spinner("Saving vector store"):
+            with open(vector_store_path, "wb") as f:
+                pickle.dump(vectorstore, f)
+        st.session_state.session_data["vectorstore"] = vectorstore
+        st.sidebar.success("Vector store updated with new file.")
+    else:
+        with st.sidebar:
+            if raw_documents:
+                with st.spinner("Splitting documents into chunks..."):
+                    text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+                    documents = text_splitter.split_documents(raw_documents)
 
-with st.sidebar:
-    # Option for using an existing vector store
-    use_existing_vector_store = st.radio("Use existing vector store if available", ["Yes", "No"], horizontal=True)
+                with st.spinner("Adding document chunks to vector database..."):
+                    vector_store_path = f"vectorstore_{str(uuid.uuid4())}.pkl"  
+                    vectorstore = FAISS.from_documents(documents, document_embedder)
+                    st.session_state.session_data["vector_file"] = vector_store_path
+                    st.session_state.session_data["vectorstore"] = vectorstore
 
-# Path to the vector store file
-vector_store_path = "vectorstore.pkl"
+                with st.spinner("Saving vector store"):
+                    with open(vector_store_path, "wb") as f:
+                        pickle.dump(vectorstore, f)
+                st.sidebar.success("Vector store created and saved.")
+            else:
+                st.sidebar.warning("No documents available to process!", icon="⚠️")
 
-# Load raw documents from the directory
-raw_documents = DirectoryLoader(DOCS_DIR).load()
-
-
-# Check for existing vector store file
-vector_store_exists = os.path.exists(vector_store_path)
-vectorstore = None
-if use_existing_vector_store == "Yes" and vector_store_exists:
-    with open(vector_store_path, "rb") as f:
-        vectorstore = pickle.load(f)
+# Document Loader
+def document_loader():
     with st.sidebar:
-        st.success("Existing vector store loaded successfully.")
-else:
-    with st.sidebar:
-        if raw_documents:
-            with st.spinner("Splitting documents into chunks..."):
-                text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
-                documents = text_splitter.split_documents(raw_documents)
+        st.subheader("Add to the Knowledge Base")
+        with st.form("my-form", clear_on_submit=True):
+            uploaded_files = st.file_uploader("Upload a file to the Knowledge Base:", accept_multiple_files=True)
+            submitted = st.form_submit_button("Upload!")
 
-            with st.spinner("Adding document chunks to vector database..."):
-                vectorstore = FAISS.from_documents(documents, document_embedder)
+        if uploaded_files and submitted:
+            if "uploaded_dir" not in st.session_state.session_data:
+                doc_dir = os.path.abspath(f"./uploaded_docs_{str(uuid.uuid4())}")
+                if not os.path.exists(doc_dir):
+                    os.makedirs(doc_dir)
+                st.session_state.session_data["uploaded_dir"] = doc_dir
+            else:
+                doc_dir = st.session_state.session_data["uploaded_dir"]
+                
+            for uploaded_file in uploaded_files:
+                st.success(f"File {uploaded_file.name} uploaded successfully!")
+                with open(os.path.join(doc_dir, uploaded_file.name), "wb") as f:
+                    f.write(uploaded_file.read())
+            session_uploaded_dir = st.session_state.session_data["uploaded_dir"]
+            
+            raw_documents = DirectoryLoader(session_uploaded_dir).load()
+            load_vectorstore(raw_documents)
 
-            with st.spinner("Saving vector store"):
-                with open(vector_store_path, "wb") as f:
-                    pickle.dump(vectorstore, f)
-            st.success("Vector store created and saved.")
+# Response Generation and Chat
+def response_generation_and_chat():
+    st.subheader("Chat with your AI Assistant, Doc'er!")
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    prompt_template = ChatPromptTemplate.from_messages(
+        [("system", "You are a helpful AI assistant named Doc'er. You will reply to questions only based on the context that you are provided. If something is out of context, you will refrain from replying and politely decline to respond to the user. You are not the person in the given context"), ("user", "{input}")]
+    )
+    user_input = st.chat_input("Can you tell me about the doc?")
+
+    chain = prompt_template | llm | StrOutputParser()
+
+    if user_input:
+        if "vectorstore" in st.session_state.session_data:
+            st.session_state.messages.append({"role": "user", "content": user_input})
+            retriever = st.session_state.session_data["vectorstore"].as_retriever()
+            docs = retriever.get_relevant_documents(user_input)
+            with st.chat_message("user"):
+                st.markdown(user_input)
+
+            context = ""
+            for doc in docs:
+                context += doc.page_content + "\n\n"
+
+            augmented_user_input = "Context: " + context + "\n\nQuestion: " + user_input + "\n"
+
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
+
+                for response in chain.stream({"input": augmented_user_input}):
+                    full_response += response
+                    message_placeholder.markdown(full_response + "▌")
+                message_placeholder.markdown(full_response)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
         else:
-            st.warning("No documents available to process!", icon="⚠️")
+            st.warning("No documents provided as context", icon="⚠️")
 
-############################################
-# Component #4 - LLM Response Generation and Chat
-############################################
+def main():
+    st.set_page_config(layout="wide")
+    initialize_session_state()
+    document_loader()
+    response_generation_and_chat()
 
-st.subheader("Chat with your AI Assistant, Envie!")
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-
-prompt_template = ChatPromptTemplate.from_messages(
-    [("system", "You are a helpful AI assistant named Envie. You will reply to questions only based on the context that you are provided. If something is out of context, you will refrain from replying and politely decline to respond to the user."), ("user", "{input}")]
-)
-user_input = st.chat_input("Can you tell me what NVIDIA is known for?")
-llm = ChatNVIDIA(model="mixtral_8x7b")
-
-chain = prompt_template | llm | StrOutputParser()
-
-if user_input and vectorstore!=None:
-    st.session_state.messages.append({"role": "user", "content": user_input})
-    retriever = vectorstore.as_retriever()
-    docs = retriever.get_relevant_documents(user_input)
-    with st.chat_message("user"):
-        st.markdown(user_input)
-
-    context = ""
-    for doc in docs:
-        context += doc.page_content + "\n\n"
-
-    augmented_user_input = "Context: " + context + "\n\nQuestion: " + user_input + "\n"
-
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-
-        for response in chain.stream({"input": augmented_user_input}):
-            full_response += response
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+if __name__ == "__main__":
+    main()
